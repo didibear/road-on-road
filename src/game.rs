3f1b14e6bcc51d1113436315e55a850d4765be11
Adrien Turiot <didibear77@gmail.com>
2024-07-21 21:33:21 -0400

    feat: add journey path management

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,27 @@
-use bevy::{prelude::*, sprite::Anchor};
+use bevy::{color::palettes::css::*, prelude::*, sprite::Anchor};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Startup, setup)
-        .add_systems(Update, (update_positions, position_to_transform, draw_grid));
+    app.add_systems(Startup, (setup_camera, spawn_player))
+        .add_systems(
+            Update,
+            (
+                update_positions,
+                position_to_transform,
+                draw_grid,
+                draw_targets,
+                draw_paths,
+            ),
+        )
+        .observe(add_new_character);
 }
 
-pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let start_pos = Position(IVec2::new(5, 5));
+    let target_pos = Position(IVec2::new(8, 8));
 
     commands.spawn((
         Player,
@@ -17,9 +32,19 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 anchor: Anchor::BottomLeft,
                 ..default()
             },
+            transform: Transform::from_translation(Vec3::from((
+                position_translation(&start_pos),
+                0.,
+            ))),
             ..default()
         },
-        Position(IVec2::new(5, 5)),
+        start_pos,
+        Journey {
+            path: Vec::from([start_pos]),
+            index: 0,
+            start_pos,
+            target_pos,
+        },
     ));
 }
 
@@ -36,22 +61,71 @@ struct Player;
 struct Automated;
 
 #[derive(Debug, Component)]
-struct Travel {
+struct Journey {
+    start_pos: Position,
+    target_pos: Position,
     path: Vec<Position>,
-    current_index: usize,
+    index: usize,
 }
 
+#[derive(Event)]
+struct JourneyFinished;
+
 fn update_positions(
+    mut commands: Commands,
     keyboard: Res<ButtonInput<KeyCode>>,
-    mut positions: Query<&mut Position, With<Player>>,
+    mut positions: Query<(Entity, &mut Position, &mut Journey), With<Player>>,
 ) {
     let Some(direction) = keyboard_direction(&keyboard) else {
         return;
     };
 
-    for mut pos in positions.iter_mut() {
-        pos.0 = (pos.0 + direction).clamp(IVec2::ZERO, GRID_SIZE.as_ivec2() - 1);
+    for (entity, mut current_pos, mut journey) in positions.iter_mut() {
+        let next_pos = (current_pos.0 + direction).clamp(IVec2::ZERO, GRID_SIZE.as_ivec2() - 1);
+
+        if next_pos != current_pos.0 {
+            current_pos.0 = next_pos;
+
+            journey.path.push(*current_pos);
+            journey.index += 1;
+
+            if journey_finished(journey) {
+                commands.trigger_targets(JourneyFinished, entity)
+            }
+        }
     }
+}
+
+fn journey_finished(journey: Mut<Journey>) -> bool {
+    let Some(target_index) = journey
+        .path
+        .iter()
+        .rposition(|pos| pos.0 == journey.target_pos.0)
+    else {
+        return false;
+    };
+    let Some(back_to_start_index) = journey
+        .path
+        .iter()
+        .rposition(|pos| pos.0 == journey.start_pos.0)
+    else {
+        return false;
+    };
+    target_index < back_to_start_index
+}
+
+fn add_new_character(
+    trigger: Trigger<JourneyFinished>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    // Current character becomes a bot
+    commands
+        .entity(trigger.entity())
+        .remove::<Player>()
+        .insert(Automated);
+
+    spawn_player(commands, asset_server)
 }
 
 fn keyboard_direction(keyboard: &Res<ButtonInput<KeyCode>>) -> Option<IVec2> {
@@ -87,12 +161,34 @@ fn draw_grid(mut gizmos: Gizmos) {
         )
         .outer_edges();
 }
+fn draw_targets(mut gizmos: Gizmos, journeys: Query<&Journey>) {
+    for journey in journeys.iter() {
+        gizmos.circle_2d(
+            position_translation(&journey.target_pos) + CELL_SIZE / 2.,
+            CELL_SIZE / 2. * 0.95,
+            RED,
+        );
+    }
+}
+fn draw_paths(mut gizmos: Gizmos, journeys: Query<&Journey>) {
+    for journey in journeys.iter() {
+        for pos in journey.path.iter() {
+            gizmos.rect_2d(
+                position_translation(pos) + CELL_SIZE / 2.,
+                0.,
+                Vec2::splat(CELL_SIZE * 0.95),
+                RED,
+            );
+        }
+    }
+}
 
 fn position_to_transform(mut positions: Query<(&mut Transform, &Position), Changed<Position>>) {
     for (mut transform, pos) in positions.iter_mut() {
-        transform.translation = Vec3::from((
-            pos.0.as_vec2() * CELL_SIZE - GRID_SIZE.as_vec2() * CELL_SIZE / 2.,
-            transform.translation.z,
-        ));
+        transform.translation = Vec3::from((position_translation(pos), 0.));
     }
+}
+
+fn position_translation(pos: &Position) -> Vec2 {
+    pos.0.as_vec2() * CELL_SIZE - GRID_SIZE.as_vec2() * CELL_SIZE / 2.
 }
